2d6c3efa49eb43d11f9760fee4d7b377d5d30804
adantop <>
2020-12-07 05:38:42 +0000

    Updates

---
+++ precache-manifest.bc8b771c5569a7f63abef8d31ea75561.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a086b4a9e13b57dbedb798f1c98e56fe",
+    "url": "/react-bootcamp-2020/index.html"
+  },
+  {
+    "revision": "9e986857976ce8306a01",
+    "url": "/react-bootcamp-2020/static/css/main.8d6a2e16.chunk.css"
+  },
+  {
+    "revision": "49aef509691059d151a1",
+    "url": "/react-bootcamp-2020/static/js/2.1cff4636.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-bootcamp-2020/static/js/2.1cff4636.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e986857976ce8306a01",
+    "url": "/react-bootcamp-2020/static/js/main.966694c9.chunk.js"
+  },
+  {
+    "revision": "eeb19e06e59dded179e1",
+    "url": "/react-bootcamp-2020/static/js/runtime-main.2724eb93.js"
+  }
+]);
